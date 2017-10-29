0c14da28551b088b7f65cb78b006c42945c22b5e
kagzouli <kagzouli@yahoo.fr>
2017-10-29 21:26:33 +0100

    KAR002 : Corrige quelques erreurs sur searchaccount

--- BanqueUserProject/BanqueUserSpringWeb/src/main/webapp/angular/exabanque/src/app/component/banque/searchbanqueaccountuser/searchbanqueaccountuser.component.ts
+++ BanqueUserProject/BanqueUserSpringWeb/src/main/webapp/angular/exabanque/src/app/component/banque/searchbanqueaccountuser/searchbanqueaccountuser.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import {ExaAccountOperation} from '../../../services/banque/exaaccountoperation';
+import {SearchAccountOperation} from '../../../services/banque/searchaccountoperationparam';
+
+import { BanqueService } from '../../../services/banque/banque.service';
+
 @Component({
   selector: 'app-searchbanqueaccountuser',
   templateUrl: './searchbanqueaccountuser.component.html',
-  styleUrls: ['./searchbanqueaccountuser.component.css']
+  styleUrls: ['./searchbanqueaccountuser.component.css'],
+  providers: [BanqueService]
 })
 export class SearchbanqueaccountuserComponent implements OnInit {
 
   rForm: FormGroup;
+  
+  listAccountOperation: ExaAccountOperation[];
 
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private banqueService: BanqueService) {
        this.rForm = fb.group({
        'identifierUser' : [null, Validators.compose([Validators.required])],
       'beginDate' : [null /*, Validators.compose([Validators.]) */],
@@ -26,6 +34,25 @@ export class SearchbanqueaccountuserComponent implements OnInit {
       console.log('Identifier user : ' + form.identifierUser);
       console.log('Begin date : ' + form.beginDate);
       console.log('End date : ' + form.endDate);
+    
+      const searchAccountOperation: SearchAccountOperation = new SearchAccountOperation(form.identifierUser, form.beginDate, form.endDate);
+    
+        // Call the search accountoperations if valid
+      if (this.rForm.valid) {
+         // Method of callback to credit account number
+         this.banqueService.findAllAccountOperation(searchAccountOperation,
+         (listAccountOperation: ExaAccountOperation[]) => {
+             this.listAccountOperation = listAccountOperation;
+           }
+         );
+
+    }else {
+         window.alert('There is a mistake in your input.');
+         // Invalid data on form - we reset.
+         this.rForm.reset();
+     }
+    
+    
 
   }
 
--- BanqueUserProject/BanqueUserSpringWeb/src/main/webapp/angular/exabanque/src/app/services/banque/banque.service.ts
+++ BanqueUserProject/BanqueUserSpringWeb/src/main/webapp/angular/exabanque/src/app/services/banque/banque.service.ts
@@ -5,22 +5,32 @@ import 'rxjs/add/operator/catch';
 import { Observable } from 'rxjs/Observable';
 
 import {AccountNumberOperation} from './accountnumberoperation';
+import {ExaAccountOperation} from './exaaccountoperation';
+import {SearchAccountOperation} from './searchaccountoperationparam';
+
+
 import {JsonResult} from '../jsonresult';
 import { HttpClient , HttpHeaders} from '@angular/common/http';
 
 
 @Injectable()
 export class BanqueService {
+  
+  contextUserServiceUrl = 'http://localhost:25000';  
 
   constructor( private http: HttpClient) { }
 
+  /**
+   * Method to credit an account for a user.
+   * 
+   */
   creditAccountNumber(accountNumberOperation: AccountNumberOperation, callback: (jsonResult: JsonResult) => void) {
 
     /*  this.http.get('http://localhost:25000/usersList')
       .map((res: Response) => res.json()).subscribe((data) => console.log(data)); */
 
      const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
-     this.http.post<JsonResult>('http://localhost:25000/creditAccount', JSON.stringify(accountNumberOperation), {headers: headers})
+     this.http.post<JsonResult>(this.contextUserServiceUrl + '/creditAccount', JSON.stringify(accountNumberOperation), {headers: headers})
        .subscribe(
         res => {
           callback(res);
@@ -30,5 +40,24 @@ export class BanqueService {
         }
       );
    }
-
+  
+  
+   /**
+   * Method to find all account operations.
+   * 
+   */  
+   findAllAccountOperation(searchaccountOperationParam: SearchAccountOperation, callback: (listAccountOperations: ExaAccountOperation[]) => void){
+        const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+        this.http.post<ExaAccountOperation[]>(this.contextUserServiceUrl + '/listOperations', JSON.stringify(searchaccountOperationParam), {headers: headers})
+       .subscribe(
+        res => {
+          callback(res);
+        },
+        err => {
+          console.log('Error occured --> ' + err);
+        }
+      );
+   }
+ 
+ 
 }
